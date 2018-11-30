9cf37d2f5efbfe7f7d65c0f4fe8be39e3b7e53ff
Mario Reiser <mario.reiser@xfel.eu>
2018-11-30 08:28:18 +0100

    test install

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from numpy.distutils.core import setup, Extension
 import os
 
 ext = [Extension(name='fecorrt3m',
-                 sources=['Xana/XpcsAna/fecorrt3m.f90'],
-                 f2py_options=['--quiet'])]
+                 sources=['Xana/XpcsAna/fecorrt3m.f90'],)]
+#                 f2py_options=['--quiet'])]
 
 setup(ext_modules=ext)
