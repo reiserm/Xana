93170a7d29a6ee184cc97e2e605ec3473332d03d
Mario Reiser <mario.reiser@xfel.eu>
2019-07-01 13:44:15 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,37 @@
 #!/usr/bin/env python
 import setuptools
-from setuptools.config import read_configuration
+from setuptools import find_packages
 from numpy.distutils.core import setup, Extension
 import os
-
-conf_dict = read_configuration('setup.cfg')
 
 ext = [Extension(name='Xana.XpcsAna.fecorrt3m',
                  sources=['Xana/XpcsAna/fecorrt3m.f90'],
                  f2py_options=['--verbose'])]
 
-setup(ext_modules=ext, **conf_dict)
+setup(
+    name = 'Xana',
+    version = '0.0.8',
+    packages=setuptools.find_packages(),
+    author = 'Mario Reiser',
+    author_email = 'mario.mkel@gmail.com',
+    url = 'https://git.xfel.eu/gitlab/reiserm/Xana.git',
+    keywords = 'analysis XPCS XSVS XFEL X-ray',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Topic :: Data Analysis',
+        'long_description = file: README.md'
+    ],
+    python_requires ='>= 3.6',
+    install_requires = [
+        'pandas',
+        'lmfit',
+        'numpy',
+        'pyfai',
+        'cbf',
+        'emcee',
+        'corner',
+        'h5py',
+    ],
+    ext_modules=ext,
+)
+
