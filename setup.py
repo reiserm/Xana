a3454f5f388a717a1cd6563bfb36e7c783d8b348
Mario Reiser <mario.reiser@xfel.eu>
2020-03-01 01:28:54 +0100

    updated setup.py: classifiers

--- setup.py
+++ setup.py
@@ -35,13 +35,12 @@ setup(
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
-        'Intended Audience :: Scientists',
+        'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Physics',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'long_description = file: README.md'
   ],
     ext_modules=ext,
 )
