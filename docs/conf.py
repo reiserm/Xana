f90d6b91a67c8ce913cc731414cf32cd0f2e4e69
Mario Reiser <mario.reiser@xfel.eu>
2020-04-19 15:26:55 +0200

    disabled mocking

--- docs/conf.py
+++ docs/conf.py
@@ -50,8 +50,6 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-autodoc_mock_imports = ["Xana.XpcsAna"]
-
 
 # -- Options for HTML output -------------------------------------------------
 
