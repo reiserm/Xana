c65b10ba8ed4d8808676b93c7b60935b5d70a48d
Mario Reiser <mario.reiser@xfel.eu>
2020-04-19 14:06:49 +0200

    deleted double kwargs

--- setup.py
+++ setup.py
@@ -27,8 +27,6 @@ setup(
     download_url = 'https://github.com/reiserm/Xana/archive/v0.0.8-alpha.tar.gz',
     keywords = ['data analysis', 'XPCS', 'XSVS', 'SAXS',],
     description="Analysis software for XPCS, XSVS and SAXS data.",
-    long_description=long_description,
-    description = "Analysis software for XPCS, XSVS and SAXS data.",
     long_description = long_description,
     long_description_content_type="text/markdown",
     python_requires ='>= 3.6',
