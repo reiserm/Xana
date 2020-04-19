fd04b57e0accd0664240ba25a187bcc9b3695316
Mario Reiser <mario.reiser@xfel.eu>
2020-04-19 14:41:13 +0200

    test automock in docs

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,9 @@
 #
 import os
 import sys
-# sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('..'))
 sys.setrecursionlimit(1500) 
-
+ 
 import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
@@ -49,6 +49,8 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+autodoc_mock_imports = ["fecorrt3m"]
 
 
 # -- Options for HTML output -------------------------------------------------
