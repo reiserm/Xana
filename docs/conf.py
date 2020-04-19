fe8b06ee39c522623c0723f87d5195912375b7e4
Mario Reiser <mario.reiser@xfel.eu>
2020-04-19 19:44:09 +0200

    updated extensions

--- docs/conf.py
+++ docs/conf.py
@@ -40,6 +40,7 @@ extensions = [
     'sphinx_rtd_theme',
     'nbsphinx',
     'm2r',
+    'IPython.sphinxext.ipython_console_highlighting',
 ]
 
 master_doc = 'index'
@@ -50,7 +51,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 
 
 # -- Options for HTML output -------------------------------------------------
