2879f40e5e510a386025e099bbefbddf63ced665
Mario Reiser <mario.mkel@gmail.com>
2022-04-21 10:08:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,13 +17,13 @@ ext_modules = cythonize(abspath("Xana/XpcsAna/cpy_ecorr.pyx"), annotate=True)
 
 setup(
     name="Xana",
-    version="0.6.6",
+    version="0.7.3",
     packages=setuptools.find_packages(),
     license="MIT",
     author="Mario Reiser",
     author_email="mario.mkel@gmail.com",
     url="https://github.com/reiserm/Xana",
-    download_url="https://github.com/reiserm/Xana/archive/v0.6.6.tar.gz",
+    download_url="https://github.com/reiserm/Xana/archive/v0.7.3.tar.gz",
     keywords=[
         "data analysis",
         "XPCS",
